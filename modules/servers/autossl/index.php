ab0752872e339c22e4f8d7f71ef2f358117fa894
Lee Mahoney <lee@leemahoney.dev>
2022-04-05 01:44:53 +0100

    Some housekeeping

---
+++ modules/servers/autossl/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * cPanel AutoSSL Module for WHMCS
+ *
+ * Create an SSL Certificate addon that utilizes cPanel's AutoSSL feature for free SSL Certificates. 
+ *
+ * @package    WHMCS
+ * @author     Lee Mahoney <lee@leemahoney.dev>
+ * @copyright  Copyright (c) Lee Mahoney 2022
+ * @license    MIT
+ * @version    0.0.1
+ * @link       https://leemahoney.dev
+ */
+
+if (!defined('WHMCS')) {
+    die('This file cannot be accessed directly');
+}
